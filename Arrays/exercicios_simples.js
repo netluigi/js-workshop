97ce1c56df26c1cf26488d235edea72538f46009
nelsonfernandes-kk <nelson.fernandes@kk.pt>
2023-03-22 21:01:06 +0000

    1º Exercicios simples com Arrays

---
+++ Arrays/exercicios_simples.js
@@ -0,0 +1,33 @@
+/* 
+Exercicios
+
+Exercício 1: Inicializar um Array
+Crie um array com um tamanho 5 e inicialize-o com valores inteiros.
+
+Exercício 2: Soma de Array
+Crie uma função que receba um array de numeros como parametro e calcule a soma de todos os elementos do Array.
+
+Exercício 3: Media de um Array
+Crie uma função que receba um array de numeros como parametro e calcule a média de todos os elementos de um array.
+
+Exercício 4: Encontra o Valor Máximo
+Crie uma função que receba um array de numeros como parametro e encontre o valor máximo dentro do array.
+
+Exercício 5: Encontra o valor mínimo
+Crie uma função que receba um array de numeros como parametro e encontre o valor mínimo de um array.
+
+Exercício 6: Inverter um Array
+Crie uma função que receba um array como parametro e Inverta os elementos do  Array.
+
+Exercício 7: Números Pares
+Crie uma função que receba um array de numeros como parametro e conte o número de números pares de um array.
+
+Exercício 8: Remover Duplicados
+Crie uma função que receba um array e remova todos os elementos duplicados e retorne o array sem os duplicados.
+
+Exercício 9: Concatenar arrays
+Crie uma função que receba dois arrays como parametro e retorne a concatenação dos 2 arrays.
+
+Exercício 10: Rotação do Array
+Crie uma função que receba um array e rode o array para a esquerda em 2 posições.
+*/
