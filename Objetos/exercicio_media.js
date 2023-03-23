035afbdf3759800ef9c2d9ce78df465b1751a357
nelsonfernandes-kk <nelson.fernandes@kk.pt>
2023-03-23 20:56:36 +0000

    Exercicio de Media

---
+++ Objetos/exercicio_media.js
@@ -0,0 +1,25 @@
+/* 
+    Exercício: Media dos Alunos
+
+    Problema:
+    Cria um array de objetos "alunos" com as propriedades "nome", "nota1" e "nota2". 
+    Em seguida, crie uma função que calcula a média de cada aluno e adiciona a propriedade "media" ao objeto.
+
+*/
+
+//Array de alunos
+let alunos = [  { nome: "João", nota1: 8, nota2: 7 },  { nome: "Maria", nota1: 9, nota2: 6 },  { nome: "Pedro", nota1: 7, nota2: 5 },  { nome: "Ana", nota1: 10, nota2: 8 }];
+
+//Your code Here
+
+
+
+/* 
+    Resultado esperado:
+    [
+    { nome: "João", nota1: 8, nota2: 7, media: 7.5 },
+    { nome: "Maria", nota1: 9, nota2: 6, media: 7.5 },
+    { nome: "Pedro", nota1: 7, nota2: 5, media: 6 },
+    { nome: "Ana", nota1: 10, nota2: 8, media: 9 }
+    ]
+*/
