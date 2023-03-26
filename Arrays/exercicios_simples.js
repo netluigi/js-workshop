a29846521f2319852c33170b4ae5e1c2633dd4e6
netluigi <luiz.anfe@hotmail.com>
2023-03-26 18:59:33 +0100

    att resolucao

--- Arrays/exercicios_simples.js
+++ Arrays/exercicios_simples.js
@@ -4,24 +4,21 @@
 // Exercício 1: Inicializar um Array
 // Crie um array com um tamanho 5 e inicialize-o com valores inteiros.
 
-const intNumbers = [1, 2, 3, 4, 5]
-
-
+const intNumbers = [1, 209, 3, 4, 10]
 
 // Exercício 2: Soma de Array
 // Crie uma função que receba um array de numeros como parametro e calcule a soma de todos os elementos do Array.
 
-function soma (arr) {
-    let somar = 0
-    for ( let i = 0; i < arr.length; i++){
-        somar += arr[i]
-    }
-    return somar;
+// function soma (arr) {
+//     let somar = 0
+//     for ( let i = 0; i < arr.length; i++){
+//         somar += arr[i]
+//     }
+//     return somar;
     
-}
-let result = soma(intNumbers);
+// }
 
-console.log(result)
+// console.log(soma(intNumbers));
 
 
 
@@ -29,14 +26,47 @@ console.log(result)
 // Exercício 3: Media de um Array
 // Crie uma função que receba um array de numeros como parametro e calcule a média de todos os elementos de um array.
 
+// function media ( arr){
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++ ){
+//         sum += arr[i];     
+//     }
+//     return sum / arr.length;
+// }
+
+// console.log(media(intNumbers))
+
 // Exercício 4: Encontra o Valor Máximo
 // Crie uma função que receba um array de numeros como parametro e encontre o valor máximo dentro do array.
+
+// function maior (arr) {
+//     const maxNum = Math.max(...arr)
+
+//     return maxNum;  
+// }
+
+// console.log(maior(intNumbers))
 
 // Exercício 5: Encontra o valor mínimo
 // Crie uma função que receba um array de numeros como parametro e encontre o valor mínimo de um array.
 
+// function menor ( arr) {
+//     const menorNum = Math.min(...arr)
+
+//     return menorNum;
+// }
+
+// console.log(menor(intNumbers))
+
 // Exercício 6: Inverter um Array
 // Crie uma função que receba um array como parametro e Inverta os elementos do  Array.
+
+function reversoArr (arr){
+    let reverso = arr.reverse()
+    return reverso
+}
+
+console.log(reversoArr(intNumbers))
 
 // Exercício 7: Números Pares
 // Crie uma função que receba um array de numeros como parametro e conte o número de números pares de um array.
