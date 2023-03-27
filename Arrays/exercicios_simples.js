068003ef7b36c9c8f1ce061cfc7d0a683c0d1185
netluigi <luiz.anfe@hotmail.com>
2023-03-27 21:15:57 +0100

    att

--- Arrays/exercicios_simples.js
+++ Arrays/exercicios_simples.js
@@ -4,7 +4,7 @@
 // Exercício 1: Inicializar um Array
 // Crie um array com um tamanho 5 e inicialize-o com valores inteiros.
 
-const intNumbers = [1, 20, 3, 4, 10]
+const intNumbers = [1, 2, 3, 4, 5 ]
 
 // Exercício 2: Soma de Array
 // Crie uma função que receba um array de numeros como parametro e calcule a soma de todos os elementos do Array.
@@ -71,23 +71,47 @@ const intNumbers = [1, 20, 3, 4, 10]
 // Exercício 7: Números Pares
 // Crie uma função que receba um array de numeros como parametro e conte o número de números pares de um array.
 
-function numPares (arr) {
-    let c = 0
-    for(let i = 0; i < arr.length; i++){
-        if ( arr[i] % 2  == 0 ){
-            c++
-        }
-    }
-    return c
-}
+// function numPares (arr) {
+//     let c = 0
+//     for(let i = 0; i < arr.length; i++){
+//         if ( arr[i] % 2  == 0 ){
+//             c++
+//         }
+//     }
+//     return c
+// }
 
-console.log(numPares(intNumbers))
+// console.log(numPares(intNumbers))
 
 // Exercício 8: Remover Duplicados
 // Crie uma função que receba um array e remova todos os elementos duplicados e retorne o array sem os duplicados.
 
+
+// function removeDuplicados (arr){
+//    let newArr = [...new Set(arr)];
+//    return newArr
+// }
+
+// console.log(removeDuplicados(intNumbers))
+
 // Exercício 9: Concatenar arrays
 // Crie uma função que receba dois arrays como parametro e retorne a concatenação dos 2 arrays.
 
+// const nome = ['Eva', 'Maria', 'Leomar']
+
+//  function juntarArrays (arrA,arrB){
+//      const result = arrA.concat(arrB)
+//      return result;
+//  }
+
+//  console.log(juntarArrays(intNumbers, nome))
 // Exercício 10: Rotação do Array
 // Crie uma função que receba um array e rode o array para a esquerda em 2 posições.
+
+// function rotation (arr) {
+//     let removed = arr.splice(0,1);
+//     arr.push(...removed);
+//     return arr
+// }
+
+// console.log(rotation(intNumbers))
